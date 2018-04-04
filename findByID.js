304a3e83904836b3cc7403d79a14d37b20b28e5f
Tammy Tian <tiantammy@outlook.com>
2018-04-05 00:40:54 +0800

    tried forEach in findById, added validateObjKeys function

--- findByID.js
+++ findByID.js
@@ -7,11 +7,16 @@ const scratchData = [
 ];
 
 function findById(items, idNum) {
-  for(let i = 0; i < items.length; i++){
-    if(items[i].id === idNum){
-      return items[i];
+  items.forEach(function (item) {
+    if(item.id === idNum){
+      return item.id;
     }
-  }
+  });
+  // for(let i = 0; i < items.length; i++){
+  //   if(items[i].id === idNum){
+  //     return items[i];
+  //   }
+  // }
 }
 
 
